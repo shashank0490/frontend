6572b10ff8134f370aa73cb6c38f075294f506a7
root <root@ip-172-31-25-229.ec2.internal>
2022-07-30 18:34:06 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>Welcome to the world of react. container volume. </p>
         <a
           className="App-link"
           href="https://reactjs.org"
